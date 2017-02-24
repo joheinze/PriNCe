d584805279681dfec146433a2ccbdaf84790e827
Anatoli Fedynitch <anatoli.fedynitch@desy.de>
2017-02-24 10:38:27 +0000

    CMB photon field and star formation rate defined

--- prince/cosmology.py
+++ prince/cosmology.py
@@ -11,7 +11,9 @@ H0 = config['H_0s']
 Omega_m = config['Omega_m']
 Omega_Lambda = config['Omega_Lambda']
 
-def H(z):
+E_CMB = 2.34823e-13  # = kB*T0 [GeV]
+
+def H(z, H0 = H0):
     """Expansion rate of the universe.
 
     :math:`H(z) = H_0 \\sqrt{\\Omega_m (1 + z)^3 + \\Omega_\\Lambda}`
@@ -22,4 +24,47 @@ def H(z):
       float: expansion rate in :math:`s^{-1}`
     """
     
-    return H0*np.sqrt(Omega_m*(1 + z)**3 + Omega_Lambda)
+    return H0 * np.sqrt(Omega_m * (1 + z) ** 3 + Omega_Lambda)
+
+def star_formation_rate(z, z_inhomogeneous=0.):
+    """Returns the star formation rate, per comoving volume, evaluated at the specified redshift.
+    
+    Ref:
+        A.M. Hopkins and J.F. Beacom, Astrophys. J. 651, 142 (2006) [astro-ph/060146]
+        P. Baerwald, S. Huemmer, and W. Winter, Astropart. Phys. 35, 508 (2012) [1107.5583]
+    
+    Args:
+      z (float): redshift
+    
+    Returns:
+      float: star formation rate in :math:`{\\rm Mpc}}^{-3}`
+    """
+      
+    if (z < z_inhomogeneous): 
+        return 0.
+    elif (z <= 0.97): 
+        return (1. + z) ** 3.44
+    elif z > 0.97 and z <= 4.48: 
+        return 10. ** 1.09 * (1. + z) ** -0.26
+    else: 
+        return 10.**6.66 * (1. + z) ** -7.8
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    
+    zrange = np.linspace(0, 6, 30)
+    sfr = np.vectorize(star_formation_rate)
+    fig, ax = plt.subplots(1, 1, figsize=(4, 3))
+    ax.plot(zrange, sfr(zrange))
+    ax.set_xlabel(r'Redshift $z$')
+    ax.set_ylabel(r'Star formation rate Mpc$^{-3}$')
+    
+    fig, ax = plt.subplots(1, 1, figsize=(4, 3))
+    ax.plot(zrange, H(zrange, H0 = config['H_0']))
+    ax.set_xlabel(r'Redshift $z$')
+    ax.set_ylabel(r'Expansion rate of the universe (km s$^{-1}$ Mpc$^{-1}$')
+    
+    plt.show()
+    
+    
--- prince/photonfields.py
+++ prince/photonfields.py
@@ -3,11 +3,61 @@ Created on Feb 22, 2017
 
 @author: Anatoli Fedynitch
 '''
-
+import numpy as np
+import cosmology as cosm
+from abc import abstractmethod
 
 class PhotonField(object):
-    def __init__(self):
-        pass
+    """Base class for constructing target photon densitites.
     
+    Derived classes have to implment the method  :func:`PhotonField.get_photon_densities`.
+    """
+    @abstractmethod
+    def get_photon_density(self, E, z):
+        raise Exception(self.__class__.__name__ + '::get_photon_density():' +
+                        'Base class method called accidentally.')
+    
+    
+class CMBPhotonSpectrum(PhotonField):
+    """Redshift-scaled number density of CMB photons
+    
+    In the CMB frame (equivalent to the observer's frame). Normalisation from Planck's spectrum. 
+    The scaling goes as :math:`n(E,z) = (1+z)^3 n(E/(1+z), z = 0)`. 
+    The CMB spectrum is a blackbody spectrum with the present-day temperature T0 = 2.725 K.                                    
 
-class CMB
+    Ref.:                                                                                    
+        M. Ahlers, L.A. Anchordoqui, and S. Sarkar, Phys. Rev. D 79, 083009 (2009) [0902.3993]    
+    """
+    
+    def get_photon_density(self, E, z):
+        """Returns the redshift-scaled number density of CMB photons
+    
+        Args:
+          z (float): redshift
+          E (float): photon energy (GeV)
+        
+        Returns:
+          float: CMB photon spectrum in :math:`{\\rm GeV}}^{-1} {\\rm cm}}^{-3}`
+        """
+                                                
+        pref = 1.31868e40  # 1/pi^2/(hbar*c)^3 [GeV^-3 cm^-3]
+        Ered = E / (1. + z)
+        # density at z = 0, for energy E / (1 + z); ECMB = kB * T0
+        nlocal = pref * Ered ** 2 / (np.exp(Ered / cosm.E_CMB) - 1.0) 
+        return (1. + z) ** 3 * nlocal
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    
+    erange = np.logspace(-20, -6, 100)
+    cmb = CMBPhotonSpectrum()
+    fig, ax = plt.subplots(1, 1, figsize=(4, 3))
+    ax.loglog(erange, erange*cmb.get_photon_density(erange, z=0.), ls='-', lw=2, color='k')
+#     ax.loglog(erange, erange*CMB_photon_spectrum(erange, z=6.), ls='--', color='k')
+    ax.set_ylim(1e-9, 1e3)
+#     ax.fill_between(erange, CMB_photon_spectrum(erange, z=0.),
+#                     CMB_photon_spectrum(erange, z=6.), color='b', alpha=0.3)
+    ax.set_xlabel(r'$\epsilon$ d$n/$d$\epsilon$ cm$^{-3}$')
+    ax.set_ylabel(r'Photon energy $\epsilon$ (GeV)')
+    
+    plt.show()
