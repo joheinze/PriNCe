78becdf38388aec48fdd6482586403711018c40e
Jonas Heinze <jonas.heinze@gmail.com>
2017-10-10 15:15:56 +0000

    Selective merge from A_branch: cr_sources.py and solvers.py

--- prince/cr_sources.py
+++ prince/cr_sources.py
@@ -5,7 +5,7 @@ from abc import ABCMeta, abstractmethod
 import numpy as np
 
 from prince.cosmology import star_formation_rate
-from prince.util import info, PRINCE_UNITS
+from prince.util import info, PRINCE_UNITS, get_AZN
 from prince_config import config
 
 
@@ -22,7 +22,6 @@ class CosmicRaySource(object):
         self.m = m
 
         self.injection_grid = np.zeros(self.prince_run.dim_states)
-
         self.inj_spec = self.spec_man.ncoid2sref[self.ncoid]
         # compute the injection rate on the fixed energy grid
         self.injection_grid[self.inj_spec.lidx():self.inj_spec.uidx(
@@ -65,13 +64,15 @@ class SimpleSource(CosmicRaySource):
     def __init__(self,
                  prince_run,
                  spectral_index=2.,
-                 emax=1e12,
+                 emax=1e13,
                  m=0.,
                  ncoid=101):
 
         self.spectral_index = spectral_index
-        self.emax = emax
         self.ncoid = ncoid
+
+        self.emax = emax/get_AZN(ncoid)[0]
+        
         CosmicRaySource.__init__(self, prince_run, m)
 
     def injection_spectrum(self, energy):
@@ -79,7 +80,7 @@ class SimpleSource(CosmicRaySource):
         power-law injection spectrum with spectral index and maximal energy cutoff
         """
         from numpy import exp
-        result = self.norm * energy**(
+        result = energy**(
             -self.spectral_index) * exp(-energy / self.emax)
 
         return result
