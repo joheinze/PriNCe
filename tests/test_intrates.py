d686ae9675202a62c0d9fcfc23f2b6173a4de38e
Jonas Heinze <jonas.heinze@gmail.com>
2020-04-30 17:29:27 +0200

    Added three unit tests

---
+++ tests/test_intrates.py
@@ -0,0 +1,45 @@
+# Test whether cross section are correctly created.
+
+import numpy as np
+
+from prince_config import config
+from prince import cross_sections, photonfields, core
+
+config["x_cut"] = 1e-4
+config["x_cut_proton"] = 1e-2
+config["tau_dec_threshold"] = np.inf
+config["max_mass"] = 14
+config["debug_level"] = 0 # suppress info statements
+
+pf = photonfields.CombinedPhotonField(
+                [photonfields.CMBPhotonSpectrum, 
+                photonfields.CIBGilmore2D])
+cs = cross_sections.CompositeCrossSection([(0., cross_sections.TabulatedCrossSection, ('CRP2_TALYS',)),
+                                        (0.14, cross_sections.SophiaSuperposition, ())])
+
+import unittest
+class TestCsec(unittest.TestCase):
+
+    def test_kernel_1(self):
+        prince_run_talys = core.PriNCeRun(max_mass = 4, photon_field=pf, cross_sections=cs)
+        self.assertEqual(prince_run_talys.int_rates._batch_matrix.shape, (88344, 72))
+        self.assertEqual(prince_run_talys.int_rates._batch_rows.shape, (88344,))
+        self.assertEqual(prince_run_talys.int_rates._batch_cols.shape, (88344,))
+        self.assertEqual(prince_run_talys.int_rates._batch_vec.shape, (88344,))
+
+    def test_kernel_4(self):
+        prince_run_talys = core.PriNCeRun(max_mass = 1, photon_field=pf, cross_sections=cs)
+        self.assertEqual(prince_run_talys.int_rates._batch_matrix.shape, (17528, 72))
+        self.assertEqual(prince_run_talys.int_rates._batch_rows.shape, (17528,))
+        self.assertEqual(prince_run_talys.int_rates._batch_cols.shape, (17528,))
+        self.assertEqual(prince_run_talys.int_rates._batch_vec.shape, (17528,))
+
+    def test_kernel_14(self):
+        prince_run_talys = core.PriNCeRun(max_mass = 14, photon_field=pf, cross_sections=cs)
+        self.assertEqual(prince_run_talys.int_rates._batch_matrix.shape, (287664, 72))
+        self.assertEqual(prince_run_talys.int_rates._batch_rows.shape, (287664,))
+        self.assertEqual(prince_run_talys.int_rates._batch_cols.shape, (287664,))
+        self.assertEqual(prince_run_talys.int_rates._batch_vec.shape, (287664,))
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_propagation.py
@@ -0,0 +1,53 @@
+# Test whether cross section are correctly created.
+
+import numpy as np
+
+from prince_config import config
+from prince import cross_sections, photonfields, core
+from prince.solvers import UHECRPropagationSolverBDF
+from prince.cr_sources import AugerFitSource
+
+config["x_cut"] = 1e-4
+config["x_cut_proton"] = 1e-2
+config["tau_dec_threshold"] = np.inf
+config["max_mass"] = 14
+config["debug_level"] = 0  # suppress info statements
+
+pf = photonfields.CombinedPhotonField(
+    [photonfields.CMBPhotonSpectrum, photonfields.CIBGilmore2D])
+cs = cross_sections.CompositeCrossSection([
+    (0., cross_sections.TabulatedCrossSection, ('CRP2_TALYS', )),
+    (0.14, cross_sections.SophiaSuperposition, ())
+])
+
+prince_run_talys = core.PriNCeRun(max_mass=14,
+                                  photon_field=pf,
+                                  cross_sections=cs)
+
+import unittest
+
+
+class TestCsec(unittest.TestCase):
+    def test_propagation(self):
+        solver = UHECRPropagationSolverBDF(initial_z=1.,
+                                           final_z=0.,
+                                           prince_run=prince_run_talys,
+                                           enable_pairprod_losses=True,
+                                           enable_adiabatic_losses=True,
+                                           enable_injection_jacobian=True,
+                                           enable_partial_diff_jacobian=True)
+
+        solver.add_source_class(
+            AugerFitSource(prince_run_talys,
+                           norm=1e-50,
+                           params={
+                               101: (0.96, 10**9.68, 20.),
+                               402: (0.96, 10**9.68, 50.),
+                               1407: (0.96, 10**9.68, 30.)
+                           }))
+        solver.solve(dz=1e-3, verbose=False, progressbar=False)
+
+        self.assertEqual(solver.known_species, prince_run_talys.spec_man.known_species)
+
+if __name__ == '__main__':
+    unittest.main()
